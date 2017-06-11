e47be9efd4f584d723c6119f0a1c6eaa3a649b55
Claus Marxmeier <claus@marxmeier.de>
2017-06-11 23:13:09 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,31 +1,30 @@
 GLUON_SITE_PACKAGES := \
     gluon-alfred \
     gluon-autoupdater \
-    gluon-banner \
-    gluon-check-client-mesh \
-    gluon-check-mesh \
     gluon-config-mode-autoupdater \
     gluon-config-mode-contact-info \
     gluon-config-mode-core \
     gluon-config-mode-geo-location \
     gluon-config-mode-hostname \
+    gluon-banner \
+    gluon-check-client-mesh \
+    gluon-check-mesh \
     gluon-config-mode-tunneldigger \
     gluon-ebtables-filter-multicast \
     gluon-ebtables-filter-ra-dhcp \
-    gluon-luci-admin \
-    gluon-luci-autoupdater \
-    gluon-luci-node-role \
-    gluon-luci-portconfig \
-    gluon-luci-private-wifi \
-    gluon-luci-wifi-config \
+    gluon-ebtables-source-filter \
+    gluon-web-admin \
+    gluon-web-autoupdater \
+    gluon-web-network \
+    gluon-web-private-wifi \
+    gluon-web-wifi-config \
     gluon-mesh-batman-adv-15 \
     gluon-mesh-vpn-tunneldigger \
     gluon-neighbour-info \
     gluon-next-node \
     gluon-radvd \
     gluon-respondd \
-    gluon-rfkill-disable \
-		gluon-setup-mode \
+    gluon-setup-mode \
     gluon-ssid-changer \
     gluon-status-page \
     gluon-tunneldigger-watchdog \
@@ -36,7 +35,8 @@ GLUON_SITE_PACKAGES := \
     gluon-rsk-config \
     gluon-treatment
 
-DEFAULT_GLUON_RELEASE := 2016.2.6-stable-2.8.1
+
+DEFAULT_GLUON_RELEASE := 2017.1-stable-2.9
 # Allow overriding the release number from the command line
 
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
