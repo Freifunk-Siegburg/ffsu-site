343d7ce408ea5e43fea5ea11df64ae58680e1a1d
Claus Marxmeier <claus@marxmeier.de>
2017-06-13 16:20:03 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,30 +1,31 @@
 GLUON_SITE_PACKAGES := \
     gluon-alfred \
     gluon-autoupdater \
-    gluon-banner \
-    gluon-check-client-mesh \
-    gluon-check-mesh \
     gluon-config-mode-autoupdater \
     gluon-config-mode-contact-info \
     gluon-config-mode-core \
     gluon-config-mode-geo-location \
     gluon-config-mode-hostname \
+    gluon-config-mode-mesh-vpn \
+    gluon-core \
+    gluon-banner \
+    gluon-check-client-mesh \
+    gluon-check-mesh \
+    gluon-ebtables \
     gluon-ebtables-filter-multicast \
     gluon-ebtables-filter-ra-dhcp \
-    gluon-luci-admin \
-    gluon-luci-autoupdater \
-    gluon-luci-node-role \
-    gluon-luci-portconfig \
-    gluon-luci-private-wifi \
-    gluon-luci-wifi-config \
+    gluon-ebtables-source-filter \
+    gluon-web-admin \
+    gluon-web-autoupdater \
+    gluon-web-network \
+    gluon-web-private-wifi \
+    gluon-web-wifi-config \
+	gluon-authorized-keys \
     gluon-mesh-batman-adv-15 \
-    gluon-authorized-keys \
     gluon-mesh-vpn-tunneldigger \
     gluon-neighbour-info \
-    gluon-next-node \
     gluon-radvd \
     gluon-respondd \
-    gluon-rfkill-disable \
     gluon-setup-mode \
     gluon-ssid-changer \
     gluon-status-page \
@@ -33,16 +34,15 @@ GLUON_SITE_PACKAGES := \
     iptables \
     iwinfo \
     rsk-nightswitch \
-    gluon-rsk-config \
-    gluon-treatment
+    gluon-rsk-config
 
 
-DEFAULT_GLUON_RELEASE := 2016.2.6-stable-2.8.2
+DEFAULT_GLUON_RELEASE := 2017.1-stable-2.9
 # Allow overriding the release number from the command line
 
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 GLUON_PRIORITY ?= 0
-GLUON_LANGS ?= en de
+GLUON_LANGS ?= de en
 GLUON_REGION ?= eu
 # Build ATH10K images
 GLUON_ATH10K_MESH ?= 11s
